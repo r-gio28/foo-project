ea5b8ec8e13b82d116d45ae8e233113c4312824c
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-02 13:30:17 -0600

    wip

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 
 const server = express()
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/api/users', (req, res) => {
     res.json([
@@ -9,6 +11,10 @@ server.get('/api/users', (req, res) => {
         { id: 2, username: 'bar'},
         { id: 3, username: 'baz'},
     ])
+})
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 8080
