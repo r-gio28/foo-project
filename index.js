c3d8be89e4e46f87d1400849434980f1d47f11f0
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-02 13:08:18 -0600

    wip

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+
+const server = express()
+
+server.get('/api/users', (req, res) => {
+    res.json([
+        { id: 1, username: 'foo'},
+        { id: 2, username: 'bar'},
+        { id: 3, username: 'baz'},
+    ])
+})
+
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
+
